c45c426871b0b79d1e4674782986a1f9270e6729
vaskovatk <vaskovatk@gmail.com>
2021-01-29 22:59:58 +0600

    sudoku 24%

--- sudoku.py
+++ sudoku.py
@@ -11,60 +11,50 @@ class Solution:
         square7 = {(6, 3), (6, 4), (6, 5), (7, 3), (7, 4), (7, 5), (8, 3), (8, 4), (8, 5), }
         square8 = {(6, 6), (6, 7), (6, 8), (7, 6), (7, 7), (7, 8), (8, 6), (8, 7), (8, 8), }
         squares = [square0, square1, square2, square3, square4, square5, square6, square7, square8]
-        freePlace = 0
-        first = True
-        while freePlace !=0 or first == True:
-            first = False
-            for i in range (0, lenght):
-                for j in range (0, lenght):
-                    if board[i][j] == ".":
-                        freePlace+=1
-                        for square in range (0, lenght):
-                            if (i, j) in squares[square]:
-                                numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-                                freePlace = self.check (i, j, numbers, board, squares[square], freePlace)
 
-    def check (self, i, j,numbers, board, square, freePlace):
-        leftNumbers = 9
+        from pip._vendor.msgpack.fallback import xrange
+        for i in xrange (0, lenght):
+            for j in xrange (0, lenght):
+                if board[i][j] != ".":
+                    for square in xrange (0, lenght):
+                        if (i, j) in squares[square]:
+                            if not self.check (i, j, {}, board, squares[square]):
+                                return False
+        return True
+
+    def check (self, i, j,numbersDict, board, square):
         for s in square:
             sI = s[0]
             sJ = s[1]
             if board[sI][sJ] != ".":
-                for n in range (0, len(numbers)):
-                    if board[sI][sJ] == numbers[n]:
-                        numbers[n] =''
-                        leftNumbers -=1
-                        break
+                if board[sI][sJ] not in numbersDict.keys():
+                    numbersDict[board[sI][sJ]] = 1
+                else:
+                    return False
+        numbersDict = {}
         tempJ = 0
         while tempJ<=8:
             if board[i][tempJ] != ".":
-                for n in range(0, len(numbers)):
-                    if board[i][tempJ] == numbers[n]:
-                        numbers[n] = ''
-                        leftNumbers -= 1
-                        break
+                if board[i][tempJ] not in numbersDict.keys():
+                    numbersDict[board[i][tempJ]] = 1
+                else:
+                    return False
             tempJ+=1
-
+        numbersDict ={}
         tempI = 0
         while tempI<=8:
             if board[tempI][j] != ".":
-                for n in range(0, len(numbers)):
-                    if board[tempI][j] == numbers[n]:
-                        numbers[n] = ''
-                        leftNumbers -= 1
-                        break
+                if board[tempI][j] not in numbersDict.keys():
+                    numbersDict[board[tempI][j]] = 1
+                else:
+                    return False
             tempI+=1
-        if leftNumbers==1:
-            freePlace -=1
-            for num in range (0, len(numbers)):
-                if numbers[num] !='':
-                    board[i][j] = numbers[num]
-        return freePlace
+        return True
 
 
 
 sol = Solution()
-sol.isValidSudoku([["5","3",".",".","7",".",".",".","."]
+print(sol.isValidSudoku([["5","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
 ,[".","9","8",".",".",".",".","6","."]
 ,["8",".",".",".","6",".",".",".","3"]
@@ -72,4 +62,5 @@ sol.isValidSudoku([["5","3",".",".","7",".",".",".","."]
 ,["7",".",".",".","2",".",".",".","6"]
 ,[".","6",".",".",".",".","2","8","."]
 ,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]])
+,[".",".",".",".","8",".",".","7","9"]]))
+print(sol.isValidSudoku([[".",".","4",".",".",".","6","3","."],[".",".",".",".",".",".",".",".","."],["5",".",".",".",".",".",".","9","."],[".",".",".","5","6",".",".",".","."],["4",".","3",".",".",".",".",".","1"],[".",".",".","7",".",".",".",".","."],[".",".",".","5",".",".",".",".","."],[".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".","."]]))
